8619f1f5c9ab3475be0d027b16178abd7afcbcc3
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2020-11-25 00:15:51 +0200

    Learned about router, controller and middleware

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const logger = require("./middleware/logger");
+const morgan = require("morgan");
+const bootcamps = require("./routes/bootcamps");
 
 //Load env vars
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// app.use(logger);
+//Dev logging middleware
+app.use(morgan("dev"));
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
